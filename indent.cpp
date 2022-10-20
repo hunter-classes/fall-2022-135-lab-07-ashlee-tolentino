40ab25cf9aa9d1eec6e3b0066986f613c2bc6e4c
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-10-20 00:01:23 -0400

    added void indent() function

--- indent.cpp
+++ indent.cpp
@@ -11,6 +11,7 @@ This program contains a function that scans the line and returns the number of o
 #include <string>
 #include <cctype>
 #include "indent.h"
+#include "unindent.h"
 
 // This function scans the line and returns the number of occurrences of the character c
 int countChar(std::string line, char c)
@@ -25,4 +26,32 @@ int countChar(std::string line, char c)
   }
   
   return result;
+}
+
+void indent()
+{
+  std::string lines2;
+  int numTabs = 0;
+
+  // puts all lines of input file into string variable lines2
+  while (std::getline(std::cin, lines2))
+  {
+    if(countChar(lines2, '}') == 1)
+    {
+      numTabs--;
+    }
+    
+    std::string result2 = removeLeadingSpaces(lines2);
+    
+    for(int i = 0; i < numTabs; i++)
+    {
+      std::cout << "\t";
+    }
+    std::cout << result2 << std::endl;
+    
+    if(countChar(lines2, '{') == 1)
+    {
+      numTabs++;
+    }
+  }
 }
