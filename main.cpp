a01f1c4ee238787ba2b71da95d2de350b468af97
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-10-20 00:06:34 -0400

    added countChar and Final Indented File Example

--- main.cpp
+++ main.cpp
@@ -4,20 +4,30 @@ Course: CSCI-135
 Instructor: Michael Zamansky
 Assignment: Lab7
 
-This program...
+This program unindents and correctly indents source code files.
 */
 
 #include <iostream>
 #include <string>
 #include <cctype>
 #include "unindent.h"
+#include "indent.h"
 
 int main()
 {
+  std::cout << "\nremoveLeadingSpaces(\"       int x = 1;  \");\n" << std::endl;
+  
   std::string result = removeLeadingSpaces("       int x = 1;  ");
   std::cout << result << std::endl;
 
+  std::cout << "\n-----------------------------\n\ncountChar(\"int main(){{{\", '{');\n" << std::endl;
 
-  
+  int result3 = countChar("int main(){{{", '{');
+  std::cout << result3 << std::endl;
+
+  std::cout << "\n-----------------------------\n\nINDENTED FILE\n" << std::endl;
+
+  indent();
+
   return 0;
 }
